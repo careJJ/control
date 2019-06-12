634891364dca86f29184519f976366f2838f52f3
careJJ <709412810@qq.com>
2019-06-12 19:52:03 +0800

    6.12

---
+++ micro/rpc/web/handler/handler.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/micro/go-micro/client"
+	example "github.com/micro/examples/template/srv/proto/example"
+)
+						//回复               传入（或理解为输出，输入）
+func ExampleCall(w http.ResponseWriter, r *http.Request) {
+	// decode the incoming request as json
+	//创建一个map，用来接收前段发过来的数据
+	var request map[string]interface{}
+	//将r.body的数据解析到request这个map当中，解析结束之后r.body的数据就空了
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// call the backend service
+	//客户端句柄        通过pb文件调用床架你客户端句柄的函数（      服务名                 客户端默认参数            ）
+	exampleClient := example.NewExampleService("go.micro.srv.template", client.DefaultClient)
+	//通过客户端句柄，调用函数 往里面传参（默认参数（上下文通讯），pb文件中的结构体）
+	rsp, err := exampleClient.Call(context.TODO(), &example.Request{
+
+		Name: request["name"].(string),//断言
+	})
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// we want to augment the response
+	//创建一个返回的map，将值赋值到map当中
+	response := map[string]interface{}{
+		"msg": rsp.Msg,
+		"ref": time.Now().UnixNano(),
+	}
+
+	// encode and write the response as json
+	//将map转换成为json发送给前端
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
